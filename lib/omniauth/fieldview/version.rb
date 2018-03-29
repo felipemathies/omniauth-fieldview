db461f3d8ad13a6d145559a2eabbd0a2285f6301
Felipe Mathies <felipe.mathies@gmail.com>
2018-03-29 19:25:30 -0300

    first structure

---
+++ lib/omniauth/fieldview/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Fieldview
+    VERSION = "0.1.0"
+  end
+end
---
+++ lib/omniauth/strategies/fieldview.rb
@@ -0,0 +1,41 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Fieldview < OmniAuth::Strategies::OAuth2
+      # Give your strategy a name.
+      option :name, :fieldview
+
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
+      option :client_options, {
+      	:site => "https://api.climate.com", 
+      	authorize_url: 'https://climate.com/static/app-login/'
+     	}
+
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
+      uid{ raw_info['id'] }
+
+      info do
+        {
+          :name => raw_info['name'],
+          :email => raw_info['email']
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/me').parsed
+      end
+    end
+  end
+end
